66b73f1fec4ab0ae6bec155f0659fc348fbd2c8c
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-08-03 11:20:49 +0900

    add BluetootLESample

--- sampleLibraryJv/src/main/java/com/example/samplelibraryjv/AndroidBluetoothLESample.java
+++ sampleLibraryJv/src/main/java/com/example/samplelibraryjv/AndroidBluetoothLESample.java
@@ -1,5 +1,17 @@
 package com.example.samplelibraryjv;
+import android.util.Log;
+
+import com.unity3d.player.UnityPlayer;
 
 public class AndroidBluetoothLESample {
     public static int sampleFunc(){return 0;}
+    public static void staticFunction(){
+        Log.d("debug-----","staticFunction" );
+        UnityPlayer.UnitySendMessage("debug-----", "staticFunction", "s2");
+    }
+
+    public void noStaticFunction(){
+        Log.d("debug-----", "noStaticFunction");
+        UnityPlayer.UnitySendMessage("debug-----", "noStaticFunction", "s2");
+    }
 }
