ab141e35fc65a51961f3c6d17fe6aeabfd1d225a
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-08-05 23:20:28 +0900

    update bluetoothLE

--- BluetoothLELib/src/main/java/com/example/bluetoothlelib/BluetoothLE.java
+++ BluetoothLELib/src/main/java/com/example/bluetoothlelib/BluetoothLE.java
@@ -1,6 +1,11 @@
 package com.example.bluetoothlelib;
+
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+
 import java.lang.Exception;
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothDevice;
@@ -11,12 +16,17 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothProfile;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.le.*;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+
 import com.unity3d.player.UnityPlayer;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.UUID;
-public class BluetoothLE
-{
+
+public class BluetoothLE {
     private static final String RECEIVE_OBJECT_NAME = "BluetoothLEReceiver";
     private static final UUID CLIENT_CHARACTERISTIC_CONFIG = UUID.fromString("566fd0bd-0e40-42fa-a0c2-3a54433220f8");
     private BluetoothAdapter adapter;
@@ -25,64 +35,75 @@ public class BluetoothLE
     private BluetoothGatt gatt;
     private BluetoothDevice device;
 
+    static final int REQUEST_CODE = 1;
+    private Context context;
+    private Activity activity;
+
+    private void checkBluetoothLEPermission() {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{
+                    Manifest.permission.CAMERA
+            }, REQUEST_CODE);
+        }
+    }
+
     // 初期化.
-    public void initialize()
-    {
+    public void initialize(Context context, Activity activity) {
         //Bluetoothアダプターを初期化
-        BluetoothManager manager = (BluetoothManager)UnityPlayer.currentActivity.getSystemService(Context.BLUETOOTH_SERVICE);
+        this.context = context;
+        this.activity = activity;
+        BluetoothManager manager = (BluetoothManager) UnityPlayer.currentActivity.getSystemService(Context.BLUETOOTH_SERVICE);
         adapter = manager.getAdapter();
 
         scanner = adapter.getBluetoothLeScanner();
 
         unitySendMessage("InitializeCallback");
     }
+
     // スキャン開始.
-    public void startScan()
-    {
+    public void startScan() {
         ScanSettings.Builder scanSettings = new ScanSettings.Builder();
         scanSettings.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY);
         ScanSettings settings = scanSettings.build();
         // NOTE: Target Android9 API28まではマニフェスト追加のみで動作 Android10以降はユーザー許可が必要.
+        checkBluetoothLEPermission();
         scanner.startScan(null, settings, scanCallback);
     }
+
+
     // スキャンの停止.
-    public void stopScan()
-    {
+    public void stopScan(Context context, Activity activity) {
+        checkBluetoothLEPermission();
         scanner.stopScan(scanCallback);
     }
 
     // デバイス接続.
-    public void connectToDevice(String address)
-    {
+    public void connectToDevice(String address) {
+        checkBluetoothLEPermission();
         device = adapter.getRemoteDevice(address);
-        if (device == null)
-        {
+        if (device == null) {
             return;
         }
-        if (gatt != null)
-        {
+        if (gatt != null) {
             gatt.disconnect();
         }
         gatt = device.connectGatt(UnityPlayer.currentActivity, true, gattCallback);
     }
 
     // デバイス接続解除.
-    public void disconnectDevice()
-    {
-        if (gatt != null)
-        {
+    public void disconnectDevice() {
+        checkBluetoothLEPermission();
+        if (gatt != null) {
             gatt.disconnect();
             gatt = null;
         }
     }
 
-    private ScanCallback scanCallback = new ScanCallback()
-    {
+    private ScanCallback scanCallback = new ScanCallback() {
         @Override
-        public void onScanResult(int callbackType, ScanResult result)
-        {
-            if(result.getDevice() == null)
-            {
+        public void onScanResult(int callbackType, ScanResult result) {
+            checkBluetoothLEPermission();
+            if (result.getDevice() == null) {
                 return;
             }
 
@@ -93,32 +114,24 @@ public class BluetoothLE
         }
     };
 
-    private BluetoothGattCallback gattCallback = new BluetoothGattCallback()
-    {
+    private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
         @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int state)
-        {
-            if (state == BluetoothProfile.STATE_CONNECTED)
-            {
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int state) {
+            if (state == BluetoothProfile.STATE_CONNECTED) {
                 // 接続成功.
                 unitySendMessage("ConnectCallback");
-            }
-            else if (state == BluetoothProfile.STATE_DISCONNECTED)
-            {
+            } else if (state == BluetoothProfile.STATE_DISCONNECTED) {
                 // 接続解除.
                 unitySendMessage("DisconnectCallback");
             }
         }
+
         @Override
-        public void onServicesDiscovered(BluetoothGatt gatt, int status)
-        {
-            if (status == BluetoothGatt.GATT_SUCCESS)
-            {
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
                 // 検出したサービスとCharacteristicを通知.
-                for (BluetoothGattService service : gatt.getServices())
-                {
-                    for (BluetoothGattCharacteristic characteristic : service.getCharacteristics())
-                    {
+                for (BluetoothGattService service : gatt.getServices()) {
+                    for (BluetoothGattCharacteristic characteristic : service.getCharacteristics()) {
                         unitySendMessage("DiscovereCharacteristicCallback", service.getUuid().toString(), characteristic.getUuid().toString());
                     }
                 }
@@ -127,14 +140,14 @@ public class BluetoothLE
     };
 
     // サービスを検出.
-    public void discoverServices()
-    {
+    public void discoverServices() {
+        checkBluetoothLEPermission();
         gatt.discoverServices();
     }
 
     // Characteristicに対してNotificationの受信を要求.
-    public void requestNotification(String serviceUUID, String notificationUUID)
-    {
+    public void requestNotification(String serviceUUID, String notificationUUID) {
+        checkBluetoothLEPermission();
         BluetoothGattService service = gatt.getService(UUID.fromString(serviceUUID));
         BluetoothGattCharacteristic characteristic = service.getCharacteristic(UUID.fromString(notificationUUID));
 
@@ -145,26 +158,22 @@ public class BluetoothLE
     }
 
     // Unity側にメッセージ通知.
-    private void unitySendMessage(String... params)
-    {
+    private void unitySendMessage(String... params) {
         String param = String.join(",", params);
         UnityPlayer.UnitySendMessage(RECEIVE_OBJECT_NAME, "PluginMessage", param);
     }
 
     // メッセージ送信.
-    public boolean sendMessage(String serviceUUID, String writeCharacteristicUUID, String message)
-    {
-        try
-        {
+    public boolean sendMessage(String serviceUUID, String writeCharacteristicUUID, String message) {
+        checkBluetoothLEPermission();
+        try {
             byte[] bytes = message.getBytes("UTF-8");
             // 書き込み.
             BluetoothGattService service = gatt.getService(UUID.fromString(serviceUUID));
             BluetoothGattCharacteristic characteristic = service.getCharacteristic(UUID.fromString(writeCharacteristicUUID));
             characteristic.setValue(bytes);
             return gatt.writeCharacteristic(characteristic);
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             unitySendMessage("ErrorCallback", e.getMessage());
         }
         return false;
